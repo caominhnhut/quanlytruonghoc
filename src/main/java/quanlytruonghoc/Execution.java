bad5fc3fddec3721640d7d6a7716870c63e070b6
Nhut Nguyen <nguyencaominhnhut@gmail.com>
2017-07-30 10:03:51 +0700

    Create structure for this software

---
+++ src/main/java/quanlytruonghoc/Execution.java
@@ -0,0 +1,10 @@
+package quanlytruonghoc;
+
+public class Execution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Day la phan mem quan ly truong hoc");
+	}
+
+}
