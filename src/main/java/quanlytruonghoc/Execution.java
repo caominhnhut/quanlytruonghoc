4803ec6d6ccea23e244d09e734600d36c56ee86e
TSWO\Thinh <trangiathinh190697@gmail.com>
2017-08-05 11:55:47 +0700

    cau hinh cho log4j

--- src/main/java/quanlytruonghoc/Execution.java
+++ src/main/java/quanlytruonghoc/Execution.java
@@ -1,10 +1,41 @@
 package quanlytruonghoc;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
 public class Execution {
+	private static final Log log = LogFactory.getLog(Execution.class);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Day la phan mem quan ly truong hoc");
+		// // TODO Auto-generated method stub
+		// System.out.println("Day la phan mem quan ly truong hoc");
+		// Logger log=Logger.getLogger(Execution.class.getName());
+		// BasicConfigurator.configure();
+		// log.debug("Hello World!");
+		//
+		// }
+
+		log.debug("Example debug message ..");
+		log.info("Example info message ..");
+		log.warn("Example warn message ..");
+		log.error("Example error message ..");
+		log.fatal("Example fatal message ..");
+
+		try {
+			raiseException();
+		} catch (Exception e) {
+			log.fatal("<Some Object>", e);
+		}
 	}
 
+	private static void raiseException() throws Exception {
+		throw new Exception("Test Exception");
+	}
 }
