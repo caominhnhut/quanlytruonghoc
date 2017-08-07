812e548a1d12936f180264ea06f85b9e8901e6e4
Nhut Nguyen <nguyencaominhnhut@gmail.com>
2017-08-08 00:06:18 +0700

    get lastest code from development branch

--- src/main/java/com/quanlytruonghoc/main/Execution.java
+++ src/main/java/com/quanlytruonghoc/main/Execution.java
@@ -1,41 +1,30 @@
-package quanlytruonghoc;
-
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Properties;
+package com.quanlytruonghoc.main;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
 
 public class Execution {
-	private static final Log log = LogFactory.getLog(Execution.class);
+
+	private static final Log logger = LogFactory.getLog(Execution.class);
 
 	public static void main(String[] args) {
-		// // TODO Auto-generated method stub
-		// System.out.println("Day la phan mem quan ly truong hoc");
-		// Logger log=Logger.getLogger(Execution.class.getName());
-		// BasicConfigurator.configure();
-		// log.debug("Hello World!");
-		//
-		// }
-
-		log.debug("Example debug message ..");
-		log.info("Example info message ..");
-		log.warn("Example warn message ..");
-		log.error("Example error message ..");
-		log.fatal("Example fatal message ..");
-
-		try {
-			raiseException();
-		} catch (Exception e) {
-			log.fatal("<Some Object>", e);
-		}
+		Execution exec = new Execution();
+		exec.runMe("Nhut Nguyen");
 	}
 
-	private static void raiseException() throws Exception {
-		throw new Exception("Test Exception");
+	private void runMe(String parameter) {
+
+		if (logger.isDebugEnabled()) {
+			logger.debug("This is debug : " + parameter);
+		}
+
+		if (logger.isInfoEnabled()) {
+			logger.info("This is info : " + parameter);
+		}
+		
+		logger.warn("This is warn : " + parameter);
+		logger.error("This is error : " + parameter);
+		logger.fatal("This is fatal : " + parameter);
+
 	}
 }
