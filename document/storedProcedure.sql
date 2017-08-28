06e73e11be2deb5708687469837193d94f22bf88
TSWO\Thinh <trangiathinh190697@gmail.com>
2017-08-28 22:30:01 +0700

    add two store procedures for loading data into hocsinh table and lop table

--- document/storedProcedure.sql
+++ document/storedProcedure.sql
@@ -15,3 +15,25 @@ DELIMITER ;
 
 call dangNhap('nguyenthilinh','3115510158');
 
+/*create store procedure lay danh sach hoc sinh*/
+DELIMITER $$
+	drop procedure if exists getHocSinh $$
+    create procedure getHocSinh()
+    begin
+		select * from nguoidung where loai_nguoi_dung='HS';
+    end; $$
+DELIMITER ;
+call getHocSinh();
+
+
+/*create store procedure lay danh sach lop*/
+DELIMITER $$
+	drop procedure if exists getTenLop $$
+    create procedure getTenLop(
+		input_magv varchar(10)
+    )
+    begin
+		select lh.tenlop from giaovien_chunhiem gvcn join lophoc lh on gvcn.malop=lh.malop where gvcn.mand=input_magv;
+    end; $$
+DELIMITER ;
+call getTenLop('GV001');
